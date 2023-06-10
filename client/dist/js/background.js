6b937c452e3f9cc839b78eab3ecde3fe4f71280c
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-06-10 21:39:59 +0900

    [add] chrome extension app (react + webpack + tailwind)

---
+++ client/dist/js/background.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+function polling() {
+    // console.log("polling");
+    setTimeout(polling, 1000 * 30);
+}
+polling();
+
+/******/ })()
+;
---
+++ client/dist/js/content_script.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.color) {
+        console.log("Receive color = " + msg.color);
+        document.body.style.backgroundColor = msg.color;
+        sendResponse("Change color to " + msg.color);
+    }
+    else {
+        sendResponse("Color message is none.");
+    }
+});
+
+/******/ })()
+;
