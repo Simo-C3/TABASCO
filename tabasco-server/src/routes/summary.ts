74744c1a5e49044e4de46510a1b4b7fb65f50dc1
Kurichi <yuuyapp080@gmail.com>
2023-06-10 12:24:45 +0900

    [chore] validate

--- tabasco-server/src/routes/summary.ts
+++ tabasco-server/src/routes/summary.ts
@@ -1,12 +1,18 @@
 import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 import { SummaryController } from '../controller/summary';
 import _summaryRepository from '../repository/openapi/summary';
 
 const summary = new Hono();
+const schema = z.object({
+	title: z.string({ required_error: 'Title is required' }),
+	text: z.string({ required_error: 'Text is required' }),
+});
 
-summary.post('/', async (c) => {
+summary.post('/', zValidator('json', schema), async (c) => {
 	const controller = new SummaryController(_summaryRepository);
-	const body = await c.req.json();
+	const body = await c.req.valid('json');
 	const result = await controller.summary(body);
 	return c.json(result);
 });
