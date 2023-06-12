57c381231f96fdbfcba052c7d3df66b56a8a4759
Kurichi <yuuyapp080@gmail.com>
2023-06-12 22:26:01 +0900

    [add] bookmark storage

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -9,6 +9,13 @@ export type BaseBookmark = {
   dateAddedUTC: string;
 };
 
+export type NewBookMark = {
+  title: string;
+  url: string | null;
+  icon: string | null;
+  parentId: number;
+};
+
 export type SidebarBucket = {
   bookmarks: BaseBookmark[];
 };
