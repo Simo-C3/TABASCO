1bb06cb2f296bf04e9c9a7c2e1bc680e87929a7e
tami0304 <hiyokotaro.1101@gmail.com>
2023-06-13 21:30:58 +0900

    :+1: アコーディオンメニューを修正

--- client/src/components/accordionMenu.tsx
+++ client/src/components/accordionMenu.tsx
@@ -6,39 +6,22 @@ type PropsType = {
 };
 
 export const AccordionMenu = ({ name, contents }: PropsType) => {
-  const childElement = useRef<HTMLDivElement>(null);
   const [showChildren, setShowChildren] = useState(false);
-  const [childHeight, setChildHeight] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    if (childElement.current) {
-      const height = childElement.current?.clientHeight;
-      setChildHeight(height);
-    }
-  }, []);
-
   const handleClick = () => {
-    if (childElement.current) {
-      setIsOpen(true);
-      if (isOpen == true) {
-        setIsOpen(false);
-      }
-    }
+    setIsOpen((prev) => !prev);
   };
 
   //配列の表示
   const SetArticle = () => {
-    if (isOpen == false) {
-      null;
-    } else if (isOpen == true)
-      return (
-        <div>
-          {contents?.map((article) => {
-            return <p>{article}</p>;
-          })}
-        </div>
-      );
+    return (
+      <div>
+        {contents?.map((article) => {
+          return <p>{article}</p>;
+        })}
+      </div>
+    );
   };
 
   return (
@@ -47,14 +30,9 @@ export const AccordionMenu = ({ name, contents }: PropsType) => {
         onClick={handleClick}
         className=" border-0 border-solid border-gray-300 bg-red-400"
       >
-        <span
-          ref={childElement}
-          className={showChildren ? "isOpen" : "isClose"}
-        >
-          {name}
-        </span>
+        <span className={showChildren ? "isOpen" : "isClose"}>{name}</span>
       </button>
-      <SetArticle />
+      {isOpen && <SetArticle />}
     </>
   );
 };
