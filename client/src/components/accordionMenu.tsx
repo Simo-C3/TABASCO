cae239e5bda1c7cb4075c536281ab246fdc2206d
tami0304 <hiyokotaro.1101@gmail.com>
2023-06-15 01:36:48 +0900

    :+1: アコーディオンメニューの変更
    開いた時にインデントされる
    アイコンの表示
    ページを押したときに外部リンクに飛ぶ

--- client/src/components/accordionMenu.tsx
+++ client/src/components/accordionMenu.tsx
@@ -1,13 +1,13 @@
-import React, { ReactNode, useEffect, useRef, useState } from 'react';
-import { Bookmarks } from '../types';
+import React, { useState } from 'react';
+import type { Bookmarks } from '../types';
 import { RootId } from '../config';
+import { Page } from './page';
 
 type PropsType = {
   contents: Bookmarks;
 };
 
 export const AccordionMenu = ({ contents }: PropsType) => {
-  const [showChildren, setShowChildren] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -17,11 +17,10 @@ export const AccordionMenu = ({ contents }: PropsType) => {
   //配列の表示
   const SetArticle = () => {
     return (
-      <div>
+      <div className='pl-5'>
         {contents.children?.map((article) => {
           if (article.type === 'page') {
-            // TODO: <Page contents={article}/> みたいに
-            return <p>{article.title}</p>;
+            return <Page contents={article} />;
           }
           return <AccordionMenu contents={article} />;
         })}
@@ -35,8 +34,8 @@ export const AccordionMenu = ({ contents }: PropsType) => {
 
   return (
     <>
-      <button onClick={handleClick} className=' border-0 border-solid border-gray-300 bg-red-400'>
-        <span className={showChildren ? 'isOpen' : 'isClose'}>{contents.title}</span>
+      <button onClick={handleClick} className=' border-0 border-solid border-gray-300 bg-gray-200 text-lg'>
+        <span className={isOpen ? 'isOpen' : 'isClose'}>{contents.title}</span>
       </button>
       {isOpen && <SetArticle />}
     </>
