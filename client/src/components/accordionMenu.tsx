12050b7d2a33db3b9bfd5bd3fab2b94bbb4e3afc
tami0304 <hiyokotaro.1101@gmail.com>
2023-06-14 21:11:49 +0900

    :+1: アコーディオンメニューの子を表示できるようにした

--- client/src/components/accordionMenu.tsx
+++ client/src/components/accordionMenu.tsx
@@ -1,11 +1,12 @@
-import React, { ReactNode, useEffect, useRef, useState } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import { Bookmarks } from '../types';
+import { RootId } from '../config';
 
 type PropsType = {
-  name: string;
-  contents?: string[];
+  contents: Bookmarks;
 };
 
-export const AccordionMenu = ({ name, contents }: PropsType) => {
+export const AccordionMenu = ({ contents }: PropsType) => {
   const [showChildren, setShowChildren] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
@@ -17,20 +18,25 @@ export const AccordionMenu = ({ name, contents }: PropsType) => {
   const SetArticle = () => {
     return (
       <div>
-        {contents?.map((article) => {
-          return <p>{article}</p>;
+        {contents.children?.map((article) => {
+          if (article.type === 'page') {
+            // TODO: <Page contents={article}/> みたいに
+            return <p>{article.title}</p>;
+          }
+          return <AccordionMenu contents={article} />;
         })}
       </div>
     );
   };
 
+  if (contents.id === RootId) {
+    return <SetArticle />;
+  }
+
   return (
     <>
-      <button
-        onClick={handleClick}
-        className=" border-0 border-solid border-gray-300 bg-red-400"
-      >
-        <span className={showChildren ? "isOpen" : "isClose"}>{name}</span>
+      <button onClick={handleClick} className=' border-0 border-solid border-gray-300 bg-red-400'>
+        <span className={showChildren ? 'isOpen' : 'isClose'}>{contents.title}</span>
       </button>
       {isOpen && <SetArticle />}
     </>
--- client/src/content_script.tsx
+++ client/src/content_script.tsx
@@ -4,14 +4,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { AccordionMenu } from './components/accordionMenu';
 import { Bookmark } from './helper/storage';
-import { BaseBookmark } from './types';
+import { BaseBookmark, Bookmarks } from './types';
+import { RootId } from './config';
 
 const Sidebar = () => {
   const [sidebarStatus, setSidebarStatus] = useState(false);
   const [bookmarks, setBookmarks] = useState<BaseBookmark[]>([]);
   const [count, setCount] = useState(0);
-  const directories = ['hogehoge', 'fugafuga', 'tamtam'];
-  const testDirectories = ['kurichi', 'shimo', 'iori'];
+  const testBookmarks: Bookmarks = {
+    id: RootId,
+    type: 'folder',
+    title: 'root',
+    children: [
+      {
+        id: 1,
+        title: 'tabasco',
+        type: 'folder',
+        children: [
+          {
+            id: 2,
+            title: 'kurichi',
+            url: 'https://google.com',
+            type: 'page',
+          },
+          {
+            id: 3,
+            title: 'tim',
+            url: 'https://twitter.com',
+            type: 'page',
+          },
+          {
+            id: 4,
+            title: 'shimomo',
+            url: 'https://youtube.com',
+            type: 'page',
+          },
+        ],
+      },
+      {
+        id: 100,
+        title: 'notion',
+        url: 'https://notion.so',
+        type: 'page',
+      },
+    ],
+  };
 
   window.addEventListener('mousemove', (e: MouseEvent) => {
     if (!sidebarStatus && window.innerWidth - e.clientX < 10) {
@@ -65,14 +102,8 @@ const Sidebar = () => {
             </div>
           );
         })}
-        <AccordionMenu name='shimomo' contents={testDirectories} />
-        <div>
-          <select>
-            {directories.map((directory) => (
-              <option value={directory}>{directory}</option>
-            ))}
-          </select>
-        </div>
+        <br />
+        <AccordionMenu contents={testBookmarks} />
       </div>
     </>
   );
