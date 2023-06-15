745478f1e249cca5a6b6bf14f1fcd512d53d3a3d
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2023-06-16 01:36:25 +0900

    [add] Feature column view & create new folder

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/popup.html",
-    "./public/options.html",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/popup.html', './public/options.html'],
   theme: {
     extend: {},
   },
