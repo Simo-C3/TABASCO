33e66a20cef4d376c21ce9e46be331cb3c2ce60f
Kurichi <yuuyapp080@gmail.com>
2023-06-16 18:07:49 +0900

    [fix] merge

--- client/src/options.tsx
+++ client/src/options.tsx
@@ -26,12 +26,6 @@ const apiShareRequest = async (id: string): Promise<Share> => {
 const Options = () => {
   const sidebar = useRef<HTMLDivElement>(null);
 
-  const sidebar = useRef<HTMLDivElement>(null);
-
-  const rerendering = (status: boolean) => {
-    ReactDOM.render(<Column rerendering={rerendering} />, column.current!);
-  };
-
   useEffect(() => {
     getShare();
   }, []);
